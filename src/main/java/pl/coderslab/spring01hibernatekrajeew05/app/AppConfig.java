5081c9ed333fb054be3015bf11bedafee3307b40
Rafal <rafal.opial@coderslab.pl>
2020-10-10 11:37:21 +0200

    Configure Hibernate for project

--- src/main/java/pl/coderslab/spring01hibernatekrajeew05/app/AppConfig.java
+++ src/main/java/pl/coderslab/spring01hibernatekrajeew05/app/AppConfig.java
@@ -3,15 +3,21 @@ package pl.coderslab.spring01hibernatekrajeew05.app;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.persistence.EntityManagerFactory;
+
 @Configuration
 @ComponentScan("pl.coderslab.spring01hibernatekrajeew05")
 @EnableWebMvc
+@EnableTransactionManagement
 public class AppConfig implements WebMvcConfigurer {
     @Bean
     public ViewResolver viewResolver() {
@@ -25,5 +31,20 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean entityManagerFactoryBean
+                = new LocalEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPersistenceUnitName("spring01hibernatekrajeew05");
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager jpaTransactionManager =
+                new JpaTransactionManager(entityManagerFactory);
+        return jpaTransactionManager;
     }
 }
---
+++ src/main/sql/db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE spring01hibernatekrajeew05
+    CHARACTER SET utf8
+    COLLATE utf8_general_ci;
