f18c84e30e57d1111bdcef2a5daa6fba22ac3ad3
Rafal <rafal.opial@coderslab.pl>
2020-10-10 15:12:03 +0200

    M6.D1.T2.Z1 Encja Book

---
+++ src/main/java/pl/coderslab/spring01hibernatekrajeew05/entity/Book.java
@@ -0,0 +1,41 @@
+package pl.coderslab.spring01hibernatekrajeew05.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private int rating;
+    private String description;
+
+    public Book(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
